709d88756a27fcb0282fbae1f6d9ff58d60153f2
LOAI MUSTAFA <73855085+Loaist12@users.noreply.github.com>
2022-06-07 21:52:07 +0300

    modify

--- ajax.php
+++ ajax.php
@@ -7,9 +7,9 @@ $data = [];
 $stmt->execute();
 $markers = $stmt->fetchAll();
 foreach ($markers as $index => $marker) {
-    $data[$index]['lat'] =  substr_replace($marker['location'], " ",strpos($marker['location'], "/"));
-    $data[$index]['lng'] =  str_replace("/", "", substr($marker['location'], strpos($marker['location'], "/")));
-    $data[$index]['name'] = $marker['governorate'];
+    $data[$marker['id']]['lat'] =  substr_replace($marker['location'], " ",strpos($marker['location'], "/"));
+    $data[$marker['id']]['lng'] =  str_replace("/", "", substr($marker['location'], strpos($marker['location'], "/")));
+    $data[$marker['id']]['name'] = $marker['governorate'];
 }
 
 echo json_encode($data);
